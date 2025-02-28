83aae65936304b7064aa9121bea632d3024552ae
Miguel José <144273867+Migueljose28@users.noreply.github.com>
2025-02-28 17:46:45 -0300

    Add files via upload

---
+++ Telas/Tela_de_sobre.py
@@ -0,0 +1,131 @@
+# AVISO: Qualquer mudança feita neste arquivo será perdida quando o pyuic5 for executado novamente.
+# Não edite este arquivo a menos que você saiba o que está fazendo.
+# Arquivo convertido em ui2py.vercel.app
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1082, 625)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_wallpaper = QtWidgets.QLabel(self.centralwidget)
+        self.label_wallpaper.setGeometry(QtCore.QRect(-40, -90, 1121, 721))
+        self.label_wallpaper.setText("")
+        self.label_wallpaper.setPixmap(QtGui.QPixmap("/Telas/Imagens/Cordel.jpg"))
+        self.label_wallpaper.setObjectName("label_wallpaper")
+        self.label_imagem_alisson = QtWidgets.QLabel(self.centralwidget)
+        self.label_imagem_alisson.setGeometry(QtCore.QRect(50, 220, 201, 221))
+        self.label_imagem_alisson.setStyleSheet("color: rgb(0, 0, 0);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_imagem_alisson.setText("")
+        self.label_imagem_alisson.setPixmap(QtGui.QPixmap("/tmp/UPLOADS/Alisson Jonas_oficial.jpg"))
+        self.label_imagem_alisson.setObjectName("label_imagem_alisson")
+        self.label_imagem_ednaldo = QtWidgets.QLabel(self.centralwidget)
+        self.label_imagem_ednaldo.setGeometry(QtCore.QRect(300, 220, 201, 221))
+        self.label_imagem_ednaldo.setStyleSheet("color: rgb(0, 0, 0);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_imagem_ednaldo.setText("")
+        self.label_imagem_ednaldo.setPixmap(QtGui.QPixmap("/tmp/UPLOADS/Jose Ednaldo_oficial.jpg"))
+        self.label_imagem_ednaldo.setObjectName("label_imagem_ednaldo")
+        self.label_imagem_miguel = QtWidgets.QLabel(self.centralwidget)
+        self.label_imagem_miguel.setGeometry(QtCore.QRect(550, 220, 201, 221))
+        self.label_imagem_miguel.setStyleSheet("color: rgb(0, 0, 0);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_imagem_miguel.setText("")
+        self.label_imagem_miguel.setPixmap(QtGui.QPixmap("/tmp/UPLOADS/Miguel José_oficial.jpg"))
+        self.label_imagem_miguel.setObjectName("label_imagem_miguel")
+        self.label_imagem_rafael = QtWidgets.QLabel(self.centralwidget)
+        self.label_imagem_rafael.setGeometry(QtCore.QRect(800, 220, 201, 221))
+        self.label_imagem_rafael.setStyleSheet("color: rgb(0, 0, 0);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_imagem_rafael.setText("")
+        self.label_imagem_rafael.setPixmap(QtGui.QPixmap("/tmp/UPLOADS/Alex Rafael_oficial.jpg"))
+        self.label_imagem_rafael.setObjectName("label_imagem_rafael")
+        self.label_nome_alisson = QtWidgets.QLabel(self.centralwidget)
+        self.label_nome_alisson.setGeometry(QtCore.QRect(65, 450, 171, 41))
+        self.label_nome_alisson.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_nome_alisson.setObjectName("label_nome_alisson")
+        self.label_nome_ednaldo = QtWidgets.QLabel(self.centralwidget)
+        self.label_nome_ednaldo.setGeometry(QtCore.QRect(315, 450, 171, 41))
+        self.label_nome_ednaldo.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_nome_ednaldo.setObjectName("label_nome_ednaldo")
+        self.label_nome_miguel = QtWidgets.QLabel(self.centralwidget)
+        self.label_nome_miguel.setGeometry(QtCore.QRect(565, 450, 171, 41))
+        self.label_nome_miguel.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_nome_miguel.setObjectName("label_nome_miguel")
+        self.label_nome_rafael = QtWidgets.QLabel(self.centralwidget)
+        self.label_nome_rafael.setGeometry(QtCore.QRect(815, 450, 171, 41))
+        self.label_nome_rafael.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_nome_rafael.setObjectName("label_nome_rafael")
+        self.pushButton_retornar = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_retornar.setGeometry(QtCore.QRect(30, 572, 171, 41))
+        self.pushButton_retornar.setStyleSheet("font: 16pt \"MS Shell Dlg 2\";")
+        self.pushButton_retornar.setObjectName("pushButton_retornar")
+        self.label_descricao = QtWidgets.QLabel(self.centralwidget)
+        self.label_descricao.setGeometry(QtCore.QRect(50, 20, 971, 161))
+        self.label_descricao.setStyleSheet("color: rgb(0, 0, 0);\n"
+"background-color: rgb(255, 255, 255);\n"
+"border-radius: 10px;\n"
+"border-style: solid;\n"
+"border-width: 5px;\n"
+"border-color: rgb(0, 0, 0);")
+        self.label_descricao.setObjectName("label_descricao")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Sobre"))
+        self.label_nome_alisson.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Alisson Jonas</span></p></body></html>"))
+        self.label_nome_ednaldo.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Jose Ednaldo</span></p></body></html>"))
+        self.label_nome_miguel.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Miguel José</span></p></body></html>"))
+        self.label_nome_rafael.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:14pt; font-weight:600;\">Alex Rafael</span></p></body></html>"))
+        self.pushButton_retornar.setText(_translate("MainWindow", "Retornar"))
+        self.label_descricao.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:12pt; font-weight:600; font-style:italic;\">O restaurante Fogo de Riba é um projeto desenvolvido por alunos do 3° Ano do curso técnico integrado </span></p><p align=\"center\"><span style=\" font-size:12pt; font-weight:600; font-style:italic;\">em informática no âmbito da disciplina de Programação com Acesso a Banco de Dados com o objetivo principal </span></p><p align=\"center\"><span style=\" font-size:12pt; font-weight:600; font-style:italic;\">de aprimorar os conhecimentos adquiridos no decorrer da disciplina, além de obter uma excelente nota na matéria.</span></p></body></html>"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
