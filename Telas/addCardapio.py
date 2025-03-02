46a0cdfeacade1e702b835f975fae7ec94821c5f
Miguel José <144273867+Migueljose28@users.noreply.github.com>
2025-03-02 18:17:32 -0300

    Add files via upload

---
+++ Telas/addCardapio.py
@@ -0,0 +1,47 @@
+import sqlite3 as db
+
+class AddBanco:
+
+    def __init__(self):
+            conn = db.connect("cardapio.db")
+            cursor = conn.cursor()
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS pratos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    categoria TEXT NOT NULL,
+                    nome TEXT NOT NULL,
+                    descrição TEXT NOT NULL     
+            )
+                    ''')
+    def adiciona_prato(self):
+            conn = db.connect("cardapio.db")
+            cursor = conn.cursor()
+            categoria = input("degite a categoria: ")
+            nome = input("digite o nome do prato novo: ")
+            descrição = input("digite a descrição do prato: ")
+            cmd = "INSERT INTO pratos VALUES (null,?, ?,?)"
+           
+            cursor.execute(cmd,(categoria, nome, descrição))
+
+            conn.commit()
+           
+            print("Deu certo")
+            cursor.close()
+            conn.close()
+
+
+    def deletar_tabela(self):
+            conn = db.connect("cardapio.db")
+            cursor = conn.cursor()
+            cursor.execute("DROP TABLE pratos")
+
+
+
+            conn.commit()
+            cursor.close()
+            conn.close()
+
+if __name__ == "__main__":
+    banco = AddBanco()
+    #banco.deletar_tabela()
+    banco.adiciona_prato()
